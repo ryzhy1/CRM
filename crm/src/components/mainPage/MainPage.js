d86fe582aaa583c62e4d2f2f23d3ad504b225801
Yaroslav <skirlyak2004@mail.ru>
2022-12-10 14:22:24 +0300

    mainPage changed

--- crm/src/components/mainPage/MainPage.js
+++ crm/src/components/mainPage/MainPage.js
@@ -5,13 +5,6 @@ import ApplicationsTable from "../application_table/ApplicationsTable";
 
 const MainPage = (props) => {
 
-    const data = [
-        {name: 'Иванов Иван', date: '01.01.2023', topic: 'Расписание фигня'},
-        {name: 'Петров Петр', date: '02.01.2023', topic: 'Колобок повесился'},
-        {name: 'Сидоров Сидр', date: '03.01.2023', topic: 'а как какать?'},
-        {name: 'Иванов Иван', date: '04.01.2023', topic: 'Тема заявки'},
-    ]
-
     return (
         <body>
             <Header name={'Заявки'}/>
